e9b124f51dc74789eb69f7255c7f4ffa856b71c6
lucasmarques2907 <lucasredra@gmail.com>
2024-05-21 21:45:14 -0300

    Estilizando a tabela, adicionando base dos modais

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="container">
+    <ModalEditUser></ModalEditUser>
+    <ModalDeleteUser></ModalDeleteUser>
         <DataTable :data="data" class="table table-hover table-striped nowrap table-bordered" :options="options">
-            <thead>
+            <thead class="collapsed">
                 <tr>
                     <th>Nome</th>
                     <th>CPF</th>
@@ -14,7 +15,6 @@
                 </tr>
             </thead>
         </DataTable>
-    </div>
 </template>
 
 <script setup>
@@ -26,40 +26,86 @@
 
 import { DataTable } from 'datatables.net-vue3';
 import DataTablesCore from 'datatables.net-bs5';
+import ModalDeleteUser from './ModalDeleteUser.vue';
+import ModalEditUser from './ModalEditUser.vue';
 
 DataTable.use(DataTablesCore);
 
+const editar = '<i class="fa-solid fa-pencil" type="button" data-bs-toggle="modal" data-bs-target="#editUser"></i>';
+const excluir = '<i class="fa-solid fa-trash" type="button" data-bs-toggle="modal" data-bs-target="#deleteUser"></i>';
+
 const data = [
-    ['Joaquim Lucas César', '626.759.075-53', '20/08/1983', 'joaquim_lucas_ramos@gmail.com', '(95) 2718-4213', 'Inativo', 'botao', 'botao2'],
-    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', 'botao', 'botao2'],
-    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', 'botao', 'botao2'],
-    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', 'botao', 'botao2'],
-    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', 'botao', 'botao2'],
-    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', 'botao', 'botao2'],
-    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', 'botao', 'botao2'],
-    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', 'botao', 'botao2'],
-    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', 'botao', 'botao2'],
-    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', 'botao', 'botao2'],
-    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', 'botao', 'botao2'],
+    ['Joaquim Lucas César', '626.759.075-53', '20/08/1983', 'joaquim_lucas_ramos@gmail.com', '(95) 2718-4213', 'Inativo', editar, excluir],
+    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', editar, excluir],
+    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', editar, excluir],
+    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', editar, excluir],
+    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', editar, excluir],
+    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', editar, excluir],
+    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', editar, excluir],
+    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', editar, excluir],
+    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', editar, excluir],
+    ['Samuel Igor Jorge Souza', '064.677.356-92', '05/10/1990', 'samuel_souza@gmail.com', '(27) 99564-0484', 'Ativo', editar, excluir],
+    ['Aparecida da Costa', '621.506.150-61', '01/12/1985', 'aparecida_costa@gmail.com', '(37) 98468-5369', 'Ativo', editar, excluir],
 ];
 
 const options = {
     language: {
-        search: 'Buscar'
+        search: "Pesquisar",
+        decimal: "",
+        emptyTable: "Sem dados disponíveis",
+        infoPostFix: "",
+        thousands: ",",
+        lengthMenu: "_MENU_ entradas por página",
+        zeroRecords: "Nenhum resultado encontrado",
     },
     layout: {
-        bottomStart: 'pageLength',
-        topStart: null
+        topStart: 'pageLength',
+        bottomStart: null,
     },
     scrollX: true,
     // scrollY: 180,
     scrollCollapse: true,
     fixedHeader: false,
     order: [],
+    lengthMenu: [
+        [5, 10, 25, 50, 100],
+        [5, 10, 25, 50, 100],
+    ],
+    pageLength: 5,
+    autoWidth: true,
 }
 
 </script>
 <style>
 @import 'bootstrap';
 @import 'datatables.net-bs5';
+
+table.dataTable th.dt-type-numeric,
+table.dataTable th.dt-type-date,
+table.dataTable td.dt-type-numeric,
+table.dataTable td.dt-type-date {
+    text-align: center;
+}
+
+.fa-pencil:hover,
+.fa-trash:hover {
+    cursor: pointer;
+}
+
+li.dt-paging-button.page-item.active a {
+    background-color: #91C141;
+    border-color: #91C141;
+    color: white !important;
+}
+
+li.dt-paging-button.page-item a.page-link {
+    color: #000000;
+}
+
+.page-item.active .page-link:focus {
+    box-shadow: 0 0px 0px 3px #90c1417e !important;
+}
+table.dataTable th, table.dataTable td{
+    box-sizing: unset;
+}
 </style>
--- frontend/src/views/Adm.vue
+++ frontend/src/views/Adm.vue
@@ -1,7 +1,9 @@
 <template>
     <Navbar></Navbar>
-    <h1 class="text-center">tela de adm</h1>
-    <DataTable></DataTable>
+    <div class="container">
+        <p class="text-start">Usuários Cadastrados</p>
+        <DataTable></DataTable>
+    </div>
     <Footer></Footer>
 </template>
 
@@ -17,6 +19,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  p{
+    font-size: 24px;
+    margin-top: 1.25rem;
+    margin-bottom: 1.25rem;
+  }
 </style>
