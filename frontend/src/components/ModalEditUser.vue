f00b5e5f0e594a6cda92127d351125edd80d1e1c
lucasmarques2907 <lucasredra@gmail.com>
2024-06-04 00:17:10 -0300

    Adicionando modal de cadastro e login da tela inicial

--- frontend/src/components/ModalEditUser.vue
+++ frontend/src/components/ModalEditUser.vue
@@ -1,39 +1,79 @@
 <template>
   <!-- Modal -->
-  <div class="modal fade" id="editarUsuario" tabindex="-1" aria-labelledby="editarUsuarioLabel" aria-hidden="true" data-bs-backdrop="static">
+  <div
+    class="modal fade"
+    id="editarUsuario"
+    tabindex="-1"
+    aria-labelledby="editarUsuarioLabel"
+    aria-hidden="true"
+    data-bs-backdrop="static"
+  >
     <div class="modal-dialog modal-dialog-centered modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h1 class="modal-title fs-5" id="editarUsuarioLabel">Dependentes</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="modal-body">
           <div class="container">
             <form class="row">
               <p class="align-start tituloDependente col-9">DEPENDENTE 1</p>
-              <i class="col-3 fa-solid fa-trash text-end align-self-center m-0"></i>
+              <i
+                class="col-3 fa-solid fa-trash text-end align-self-center m-0"
+              ></i>
               <div class="col-lg-6 col-md-12 mt-2">
-                <label for="nomeCompleto1" class="form-label">Nome completo</label>
-                <input type="text" class="form-control" id="nomeCompleto1" placeholder="Lucas Vinícius Marques">
+                <label for="nomeCompleto1" class="form-label"
+                  >Nome completo</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="nomeCompleto1"
+                  placeholder="Lucas Vinícius Marques"
+                />
               </div>
               <div class="col-lg-6 col-md-6 mt-2">
                 <label for="email1" class="form-label">E-mail</label>
-                <input type="email" class="form-control" id="email1" placeholder="lucas@gmail.com">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email1"
+                  placeholder="lucas@gmail.com"
+                />
               </div>
               <div class="col-lg-4 col-md-6 mt-2">
-                <label for="parentesco1" class="form-label">Grau de parentesco</label>
-                <input type="text" class="form-control" id="parentesco1">
+                <label for="parentesco1" class="form-label"
+                  >Grau de parentesco</label
+                >
+                <input type="text" class="form-control" id="parentesco1" />
               </div>
               <div class="col-lg-4 col-md-6 mt-2">
                 <label for="cpf1" class="form-label">CPF</label>
-                <input type="text" class="form-control" id="cpf1" placeholder="000.000.000-00">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="cpf1"
+                  placeholder="000.000.000-00"
+                />
               </div>
               <div class="col-lg-4 col-md-6 mt-2">
                 <label for="telefone1" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="telefone1" placeholder="(DDD) 00000-0000">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="telefone1"
+                  placeholder="(DDD) 00000-0000"
+                />
               </div>
               <div class="w-100 text-center">
-                <button type="button" class="mt-2 btnAdicionar"><i class="fa-solid fa-plus"></i> Adicionar</button>
+                <button type="button" class="mt-2 btnAdicionar">
+                  <i class="fa-solid fa-plus"></i> Adicionar
+                </button>
               </div>
             </form>
           </div>
@@ -41,7 +81,9 @@
         <div class="modal-footer justify-content-center">
           <button type="button" class="btnSalvar">SALVAR</button>
           <div class="espacoBotoes"></div>
-          <button type="button" class="btnCancelar" data-bs-dismiss="modal">CANCELAR</button>
+          <button type="button" class="btnCancelar" data-bs-dismiss="modal">
+            CANCELAR
+          </button>
         </div>
       </div>
     </div>
@@ -50,38 +92,36 @@
 
 <script>
 export default {
-  name: 'modalEditUser',
-  components: {
-
-  },
-}
+  name: "modalEditUser",
+  components: {},
+};
 </script>
 
 <style>
-.tituloDependente{
+.tituloDependente {
   font-size: 18px;
   font-weight: bold;
   margin-bottom: 0;
 }
 
-input{
+input {
   text-overflow: ellipsis;
 }
 
-.btnAdicionar{
+.btnAdicionar {
   width: 110px;
-  background-color: #FFF;
+  background-color: #fff;
   border: 1px solid #fff;
   transition: all 0.25s;
 }
 
-.btnAdicionar:hover{
+.btnAdicionar:hover {
   border-color: black;
 }
 
 .btnSalvar {
-  background-color: #91C141 !important;
-  color: #FFF;
+  background-color: #91c141 !important;
+  color: #fff;
   font-weight: bold;
   width: 110px;
   padding: 5px 0;
@@ -90,7 +130,7 @@ input{
   transition: all 0.25s;
 }
 
-.btnSalvar:hover{
+.btnSalvar:hover {
   border-color: black;
 }
 
@@ -104,7 +144,7 @@ input{
   transition: all 0.25s;
 }
 
-.btnCancelar:hover{
+.btnCancelar:hover {
   border-color: black;
 }
 
--- frontend/src/components/ModalHome.vue
+++ frontend/src/components/ModalHome.vue
@@ -1,58 +1,232 @@
 <template>
-    <div class="modal fade" id="exampleModalToggle" data-bs-backdrop="static" aria-hidden="true"
-        aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Modal 1</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Show a second modal and hide this one with the button below.
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open
-                        second
-                        modal</button>
-                </div>
-            </div>
+  <div
+    class="modal fade"
+    id="exampleModalToggle"
+    data-bs-backdrop="static"
+    aria-hidden="true"
+    aria-labelledby="exampleModalToggleLabel"
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalToggleLabel">
+            Acesse sua Conta
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
-    </div>
-    <div class="modal fade" id="exampleModalToggle2" data-bs-backdrop="static" aria-hidden="true"
-        aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Hide this modal and show the first with the button below.
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back to
-                        first</button>
-                </div>
-            </div>
+        <div class="modal-body">
+          <div class="container">
+            <form class="row justify-content-center">
+              <div class="col-lg-9 col-md-12 mt-2">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="emailLogin"
+                  placeholder="Email*"
+                />
+              </div>
+              <div class="col-lg-9 col-md-12 mt-2">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="senhaLogin"
+                  placeholder="Senha*"
+                />
+              </div>
+              <div class="col-lg-9 col-md-12">
+                <button type="submit" class="btnSalvar w-100 mt-3 mb-3">
+                  ENTRAR
+                </button>
+              </div>
+              <a class="text-center mb-3 esqueceuSenha" href="#"
+                >Esqueceu sua senha?</a
+              >
+            </form>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
+  <div
+    class="modal fade"
+    id="exampleModalToggle2"
+    data-bs-backdrop="static"
+    aria-hidden="true"
+    aria-labelledby="exampleModalToggleLabel2"
+    tabindex="-1"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">
+            Cadastro
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="container">
+            <form class="row">
+              <p class="align-start tituloDependente col-9">Titular</p>
+              <div class="col-lg-12 col-md-12 mt-2">
+                <label for="nomeCompletoTitular" class="form-label"
+                  >Nome completo</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="nomeCompletoTitular"
+                  placeholder="Lucas Vinícius Marques"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="cpfTitular" class="form-label">CPF</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="cpfTitular"
+                  placeholder="000.000.000-00"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="telefoneTitular" class="form-label">Telefone</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="telefoneTitular"
+                  placeholder="(DDD) 00000-0000"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="nascimentoTitular" class="form-label"
+                  >Data de nascimento</label
+                >
+                <input
+                  type="date"
+                  class="form-control"
+                  id="nascimentoTitular"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="emailTitular" class="form-label">E-mail</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="emailTitular"
+                  placeholder="lucas@gmail.com"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="senhaTitular" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="senhaTitular" />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="repeteSenhaTitular" class="form-label"
+                  >Confirmação da senha</label
+                >
+                <input
+                  type="password"
+                  class="form-control"
+                  id="repeteSenhaTitular"
+                />
+              </div>
+              <p class="align-start tituloDependente col-9 mt-5">DEPENDENTE 1</p>
+              <div class="col-lg-6 col-md-12 mt-2">
+                <label for="nomeCompleto1" class="form-label"
+                  >Nome completo</label
+                >
+                <input
+                  type="text"
+                  class="form-control"
+                  id="nomeCompleto1"
+                  placeholder="Lucas Vinícius Marques"
+                />
+              </div>
+              <div class="col-lg-6 col-md-6 mt-2">
+                <label for="email1" class="form-label">E-mail</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email1"
+                  placeholder="lucas@gmail.com"
+                />
+              </div>
+              <div class="col-lg-4 col-md-6 mt-2">
+                <label for="parentesco1" class="form-label"
+                  >Grau de parentesco</label
+                >
+                <input type="text" class="form-control" id="parentesco1" />
+              </div>
+              <div class="col-lg-4 col-md-6 mt-2">
+                <label for="cpf1" class="form-label">CPF</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="cpf1"
+                  placeholder="000.000.000-00"
+                />
+              </div>
+              <div class="col-lg-4 col-md-6 mt-2">
+                <label for="telefone1" class="form-label">Telefone</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="telefone1"
+                  placeholder="(DDD) 00000-0000"
+                />
+              </div>
+              <div class="w-100 text-center">
+                <button type="button" class="mt-4 btnAdicionar">
+                  <i class="fa-solid fa-plus"></i> Adicionar
+                </button>
+                <div class="col-lg-12 col-md-12 mt-4 mb-4 justify-content-center d-flex">
+                  <button type="submit" class="btnSalvar">CADASTRAR</button>
+                  <div class="espacoBotoes"></div>
+                  <button
+                    type="button"
+                    class="btnCancelar"
+                    data-bs-dismiss="modal"
+                  >
+                    CANCELAR
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'modalHome',
-    components: {
-
-    },
-}
+  name: "modalHome",
+  components: {},
+};
 </script>
 
-<style>
+<style scoped>
 .modal-header {
-    border-bottom: none !important;
+  border-bottom: none !important;
 }
 
 .modal-footer {
-    border-top: none !important;
+  border-top: none !important;
+}
+
+.esqueceuSenha {
+  text-decoration: none;
+  color: black;
 }
 </style>
